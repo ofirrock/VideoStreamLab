13fddce8617665e44631a641f5b07e1cce09e496
Ofir Rock <ofirrock4g@gmail.com>
2018-07-24 12:05:53 +0300

    recorder test demo
    recorder test demo using pygame

---
+++ recorder/client.py
@@ -0,0 +1,69 @@
+#! python 3
+from socket import socket
+from zlib import decompress
+import lz4.frame
+import lz4framed
+
+import pygame
+from pygame.locals import *
+
+WIDTH = 640
+HEIGHT = 480
+
+
+def recvall(conn, length):
+    """ Retreive all pixels. """
+
+    buf = b''
+    while len(buf) < length:
+        data = conn.recv(length - len(buf))
+        if not data:
+            return data
+        buf += data
+    return buf
+
+
+def main(host='127.0.0.1', port=5000):
+    pygame.init()
+    FPS_FONT = pygame.font.SysFont("Verdana", 20)
+    GOLDENROD = pygame.Color("goldenrod")
+
+    def show_fps(window, clock):
+        pygame.draw.rect(window, (0, 0, 0), Rect((0, 0), (60, 30)))
+        fps_overlay = FPS_FONT.render(
+            "{0:.2f}".format(clock.get_fps()), True, GOLDENROD)
+        window.blit(fps_overlay, (0, 0))
+
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+    watching = True
+
+    sock = socket()
+    sock.connect((host, port))
+    try:
+        while watching:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    watching = FalseE
+                    break
+
+            # Retreive the size of the pixels length,
+            # the pixels length and pixels
+            size_len = int.from_bytes(sock.recv(1), byteorder='big')
+            size = int.from_bytes(sock.recv(size_len), byteorder='big')
+            pixels = lz4framed.decompress(recvall(sock, size))
+
+            # Create the Surface from raw pixels
+            img = pygame.image.fromstring(pixels, (WIDTH, HEIGHT), 'RGB')
+
+            # Display the picture
+            screen.blit(img, (0, 0))
+            show_fps(screen, clock)
+            pygame.display.flip()
+            clock.tick(60)
+    finally:
+        sock.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ recorder/server.py
@@ -0,0 +1,58 @@
+#! python 3
+from socket import socket
+from threading import Thread
+from zlib import compress
+import lz4.frame
+import lz4framed
+
+from mss import mss
+
+
+WIDTH = 640
+HEIGHT = 480
+
+
+def retreive_screenshot(conn):
+    with mss() as sct:
+        # The region to capture
+        rect = {'top': 0, 'left': 0, 'width': WIDTH, 'height': HEIGHT}
+
+        while 'recording':
+            # Capture the screen
+            img = sct.grab(rect)
+            # Tweak the compression level here (0-9)
+            pixels = lz4framed.compress(img.rgb)  # compress(img.rgb, 6)
+
+            print(img.rgb)
+
+            # Send the size of the pixels length
+            size = len(pixels)
+            size_len = (size.bit_length() + 7) // 8
+            conn.send(bytes([size_len]))
+
+            # Send the actual pixels length
+            size_bytes = size.to_bytes(size_len, 'big')
+            conn.send(size_bytes)
+
+            # Send pixels
+            conn.sendall(pixels)
+
+
+def main(host='0.0.0.0', port=5000):
+    sock = socket()
+    sock.bind((host, port))
+    try:
+        sock.listen(5)
+        print('Server started.')
+
+        while 'connected':
+            conn, addr = sock.accept()
+            print('Client connected IP:', addr)
+            thread = Thread(target=retreive_screenshot, args=(conn,))
+            thread.start()
+    finally:
+        sock.close()
+
+
+if __name__ == '__main__':
+    main()
