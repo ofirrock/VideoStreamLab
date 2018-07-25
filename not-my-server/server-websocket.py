a5f1344a189ec23921d771b8ea75e7dd3faeaa94
Ofir Rock <ofirrock4g@gmail.com>
2018-07-25 10:40:11 +0300

    formatting and small refactoring of server-websocket

--- not-my-server/server-websocket.py
+++ not-my-server/server-websocket.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul 24 13:15:47 2018
+Created on Tue Jul 24 2018
 
-@author: ofek
+@authors: Ofek Baba & Ofir Rock
 """
 
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
@@ -10,40 +10,46 @@ import json
 from threading import Thread, Event
 import time
 
+SERVER_ADDRESS = ''  # '' = 0.0.0.0 -> all IPv4 addresses on the local machine
+SERVER_PORT = 8003  # low-level api socket method to find open port = 0
 
-class StoppableThread(Thread):
-    """Thread class with a stop() method. The thread itself has to check
-    regularly for the stopped() condition."""
 
-    def __init__(self, client):
-        super(StoppableThread, self).__init__()
+class WebSocketStoppableThread(Thread):
+    """
+    Thread class with a stop() method.
+    The thread itself has to check regularly for the stopped() condition.
+    """
+
+    def __init__(self, webSocketClient):
+        super(WebSocketStoppableThread, self).__init__()
         self._stop_event = Event()
-        self.client = client
+        self.webSocketClient = webSocketClient
 
     def stop(self):
         self._stop_event.set()
 
-    def stopped(self):
+    def is_stopped(self):
         return self._stop_event.is_set()
 
     def run(self):
-        print("thread started for", self.client.address)
+        print("thread started for: ", self.webSocketClient.address)
         messageData = {}
         messageData['movement'] = 3
-        while not self.stopped():
+        while not self.is_stopped():
             try:
-                print(self.client.state)
+                print(self.webSocketClient.state)
                 time.sleep(3)
-                self.client.sendMessage(json.dumps(messageData))
+                self.webSocketClient.sendMessage(json.dumps(messageData))
             except:
                 print("a thread died")
-                self.client.close()
-
-
-clients = {}
+                self.webSocketClient.close()
 
 
 class SimpleEcho(WebSocket):
+
+    def __init__(self):
+        super(WebSocket, self).__init__()
+        self.clients = {}
 
     def handleMessage(self):
         # echo message back to client
@@ -51,14 +57,15 @@ class SimpleEcho(WebSocket):
 
     def handleConnected(self):
         print(self.address, 'connected')
-        thread = StoppableThread(self)
-        clients[self.address] = {'socket': self, 'handler': thread}
-        thread.start()
+        webSocketHandlerThread = WebSocketStoppableThread(webSocketClient=self)
+        self.clients[self.address] = {'websocket': self,
+                                      'handler': webSocketHandlerThread}
+        webSocketHandlerThread.start()
 
     def handleClose(self):
         print(self.address, 'closed')
-        clients[self.address]['handler'].stop()
-#        del clients[self.address]
+        self.clients[self.address]['handler'].stop()
+#        del self.clients[self.address]
 
 # def handleClient(client):
 #    try:
@@ -74,5 +81,8 @@ class SimpleEcho(WebSocket):
 #        client.close()
 
 
-server = SimpleWebSocketServer('', 8003, SimpleEcho)
+server = SimpleWebSocketServer(SERVER_ADDRESS, SERVER_PORT, SimpleEcho)
+print('websocket-server initialized at ws://%s:%s'
+      % ('0.0.0.0' if (SERVER_ADDRESS == '') else SERVER_ADDRESS,
+         '<open-port-internal>' if (SERVER_PORT == 0) else SERVER_PORT))
 server.serveforever()
